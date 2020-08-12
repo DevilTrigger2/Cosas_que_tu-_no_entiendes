fa9d84dee1eaf324f33da6bab19a6350fc9d1e95
adriandeadly <67083637+adriandeadly@users.noreply.github.com>
2020-08-11 23:13:50 -0500

    Add files via upload

---
+++ Docente/index.php
@@ -0,0 +1,34 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Inicio</title>
+	<link rel="stylesheet" href="styles.css">
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="sidebar">
+        <h2>Help Ticks</h2>
+        <ul>
+            <li><a href="#"><i class="fas fa-home"></i>Inicio</a></li>
+            <li><a href="#"><i class="fas fa-user"></i>Perfil</a></li>
+            <li><a href="#"><i class="fas fa-file-alt"></i>Ticks</a></li>
+            <li><a href="#"><i class="fas fa-user-friends"></i>Sustituto</a></li>
+            <li><a href="logout.php"><i class="fas fa-sign-out-alt"></i>Cerrar Sesion</a></li>
+        </ul> 
+
+    </div>
+    <div class="main_content">
+        <div class="header">Welcome!! Have a nice day.</div>  
+        <div class="info">
+
+      </div>
+    </div>
+</div>
+
+</body>
+</html>
